ba1214e214fc0c56ea4540cdc4c0f01e6e1813ec
An Thy <anthytran7@gmail.com>
2020-07-19 01:13:36 +1000

    Product descriptions

--- frontend/src/samples/shops.js
+++ frontend/src/samples/shops.js
@@ -39,22 +39,26 @@ const shop1 = {
     {
       name: 'Bath Salt',
       image: bath1,
+      description: 'Add to your bath for a relaxing experience. 100g of product.',
       price: 6.50
     },
     {
       name: 'Bath Tea',
       image: bath2,
+      description: 'Dunk into your bath for a relaxing experience. Scented with yellow roses.',
       price: 8.20
     },
     {
       name: 'Pink Bar',
       image: bath3,
+      description: 'The product that made us big. Soap scented with pink roses from my garden and naturally coloured with pink roses. 100g of product.',
       price: 7.00
     },
     {
       name: 'Soap',
       image: bath4,
-      price: 7.00
+      description: 'Regular old unscented soap! 100g of product.',
+      price: 5.00
     }
   ]
 }
@@ -65,21 +69,25 @@ const shop2 = {
     {
       name: 'Magenta Flowers',
       image: florist1,
-      price: 29.00
+      description: 'An assortment of magenta-coloured chrysanthemums. Popular for Mother\'s day',
+      price: 35.00
     },
     {
       name: 'Orange Flowers',
       image: florist2,
+      description: 'An assortment of orange-coloured flowers. Guaranteed to bring a smile to anyone\'s face',
       price: 28.00
     },
     {
       name: 'Pink Flowers',
       image: florist3,
-      price: 27.00
+      description: 'A gorgeous arrangement of pink flowers. Bound to make a statement.',
+      price: 39.00
     },
     {
       name: 'Sunflowers',
       image: florist4,
+      description: '7 sunflowers.',
       price: 20.00
     }
   ]
@@ -91,21 +99,25 @@ const shop3 = {
     {
       name: 'Blue Cup',
       image: pottery1,
+      description: 'Brilliant blue cup with purple accents.',
       price: 20.00
     },
     {
       name: 'Purple Pot',
       image: pottery2,
+      description: 'Intense purple cup with yellow accents.',
       price: 20.00
     },
     {
       name: 'White Pot',
       image: pottery3,
+      description: 'Stunning white pot with intricate details.',
       price: 20.00
     },
     {
       name: 'Yellow Pot',
       image: pottery4,
+      description: 'Happy yellow pot adorned with white lily details.',
       price: 20.00
     }
   ]
@@ -117,31 +129,37 @@ const shop4 = {
     {
       name: 'Brown Teddy Bear',
       image: toys1,
+      description: 'Classic brown teddy bear.',
       price: 15.00
     },
     {
       name: 'Bunny',
       image: toys2,
+      description: 'Quirky white bunny.',
       price: 17.50
     },
     {
       name: 'Giraffe',
       image: toys3,
+      description: 'Cheeky pink giraffe.',
       price: 12.50
     },
     {
       name: 'Graduation Bear',
       image: toys4,
+      description: 'A happy bear for a happy graduate.',
       price: 18.00
     },
     {
       name: 'Koala Bear',
       image: toys5,
+      description: 'Not really a bear.',
       price: 19
     },
     {
       name: 'Owl',
       image: toys6,
+      description: 'Ted\'s unusually long bear',
       price: 16
     }
   ]
@@ -153,25 +171,66 @@ const shop5 = {
     {
       name: 'Honeymead\'s honey',
       image: honey1,
-      price: 14.00
+      description: 'The most fresh, local honey you can get! 150g pure honey',
+      price: 7.50
     },
     {
       name: 'Honeymead\'s honey gel',
       image: honey2,
+      description: 'Honey to treat stubborn blemishes on your skin. 10ml of product.',
       price: 9.00
     },
     {
       name: 'Honeymead\'s vanilla honey',
       image: honey3,
-      price: 12.00
+      description: 'Honeymead\'s fresh honey, infused with organic vanilla bean. 150g of product.',
+      price: 12.50
     },
     {
       name: 'Honeymead\'s honey mask',
       image: honey4,
-      price: 16.00
+      description: 'Freshing, moisturising mask treatment for your skin. 200g of product.',
+      price: 17.00
+    }
+  ]
+}
+
+const shop6 = {
+  name: 'Bready for anything',
+  description: 'We sell bread. Contact Brandon for enquiries.',
+  products: [
+    {
+      name: 'Donut',
+      image: bakery1,
+      description: 'Frosty donut.',
+      price: 3.00
+    },
+    {
+      name: 'Loaf',
+      image: bakery2,
+      description: 'Loaf of bread.',
+      price: 3.00
+    },
+    {
+      name: 'Bread rolls',
+      image: bakery3,
+      description: 'Pack of four fresh bread rolls.',
+      price: 3.00
+    },
+    {
+      name: 'Cheesy bread bomb',
+      image: bakery4,
+      description: 'We got this idea from a Korean street market',
+      price: 4.00
+    },
+    {
+      name: 'Sourdough bread',
+      image: bakery5,
+      description: 'You said you\'d start making this right? Now you don\'t have to!',
+      price: 4.00
     }
   ]
 }
 export const shops = [
-  shop1, shop2, shop3, shop4, shop5
+  shop1, shop2, shop3, shop4, shop5, shop6
 ]
