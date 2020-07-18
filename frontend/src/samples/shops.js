6ca6eacea539a32abdb4769544174c63e762b2ab
An Thy <anthytran7@gmail.com>
2020-07-19 00:51:53 +1000

    Shops and descriptions

--- frontend/src/samples/shops.js
+++ frontend/src/samples/shops.js
@@ -21,103 +21,118 @@ import toys4 from '../images/toys/grad_teddy.png'
 import toys5 from '../images/toys/koala.png'
 import toys6 from '../images/toys/owl.png'
 
+import honey1 from '../images/honey/honey.png'
+import honey2 from '../images/honey/honeygel.png'
+import honey3 from '../images/honey/jar.png'
+import honey4 from '../images/honey/mask.png'
+
+import bakery1 from '../images/bakery/donut.png'
+import bakery2 from '../images/bakery/load.png'
+import bakery3 from '../images/bakery/rolls.png'
+import bakery4 from '../images/bakery/round.png'
+import bakery5 from '../images/bakery/sourdough.png'
+
 const shop1 = {
   name: 'Rose Apothacery',
+  description: 'Hi, Rose here! I sell fun things in your baths in this shop. Some things you might enjoy are the rose scents from my back garden!',
   products: [
     {
       name: 'Bath Salt',
       image: bath1,
-      price: 6
+      price: 6.50
     },
     {
       name: 'Bath Tea',
       image: bath2,
-      price: 8
+      price: 8.20
     },
     {
       name: 'Pink Bar',
       image: bath3,
-      price: 7
+      price: 7.00
     },
     {
       name: 'Soap',
       image: bath4,
-      price: 7
+      price: 7.00
     }
   ]
 }
 const shop2 = {
   name: 'Sophie\'s Flowers',
+  description: 'We stock an assortment of flowers. Our most popular for July are lilies, hyacinths and cosmos! We offer custom gift wrapping and can fulfil any request. Just shoot us a message on 0428 288 123',
   products: [
     {
       name: 'Magenta Flowers',
       image: florist1,
-      price: 29
+      price: 29.00
     },
     {
       name: 'Orange Flowers',
       image: florist2,
-      price: 28
+      price: 28.00
     },
     {
       name: 'Pink Flowers',
       image: florist3,
-      price: 27
+      price: 27.00
     },
     {
       name: 'Sunflowers',
       image: florist4,
-      price: 20
+      price: 20.00
     }
   ]
 }
 const shop3 = {
   name: 'The Smiths',
+  description: 'Shop of Diana and Pierre Smith',
   products: [
     {
       name: 'Blue Cup',
       image: pottery1,
-      price: 20
+      price: 20.00
     },
     {
       name: 'Purple Pot',
       image: pottery2,
-      price: 20
+      price: 20.00
     },
     {
       name: 'White Pot',
       image: pottery3,
-      price: 20
+      price: 20.00
     },
     {
       name: 'Yellow Pot',
       image: pottery4,
-      price: 20
+      price: 20.00
     }
   ]
 }
 const shop4 = {
   name: 'Teds Teddies',
+  description: 'Custom teddy bears. Contact for personalisation.',
   products: [
     {
       name: 'Brown Teddy Bear',
       image: toys1,
-      price: 15
+      price: 15.00
     },
     {
       name: 'Bunny',
       image: toys2,
-      price: 17
+      price: 17.50
     },
     {
       name: 'Giraffe',
       image: toys3,
-      price: 12
+      price: 12.50
     },
     {
       name: 'Graduation Bear',
-      image: toys4
-      price: 18
+      image: toys4,
+      price: 18.00
     },
     {
       name: 'Koala Bear',
@@ -131,7 +146,32 @@ const shop4 = {
     }
   ]
 }
-
+const shop5 = {
+  name: 'Honeymead\'s',
+  description: 'Specialty honey products for sale, right from bees in my back garden! Bees travel up to 6km away to gather their honey, so some of this honey might be thanks to the flowers in your back garden!',
+  products: [
+    {
+      name: 'Honeymead\'s honey',
+      image: honey1,
+      price: 14.00
+    },
+    {
+      name: 'Honeymead\'s honey gel',
+      image: honey2,
+      price: 9.00
+    },
+    {
+      name: 'Honeymead\'s vanilla honey',
+      image: honey3,
+      price: 12.00
+    },
+    {
+      name: 'Honeymead\'s honey mask',
+      image: honey4,
+      price: 16.00
+    }
+  ]
+}
 export const shops = [
-  shop1, shop2, shop3, shop4
+  shop1, shop2, shop3, shop4, shop5
 ]
