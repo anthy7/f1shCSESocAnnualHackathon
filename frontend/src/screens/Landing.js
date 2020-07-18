0390794b0d7043ec8bd4e60177e235676a2f656e
nzh20 <nancyzhao999@gmail.com>
2020-07-18 20:40:50 +1000

    Landing page

--- frontend/src/screens/Landing.js
+++ frontend/src/screens/Landing.js
@@ -15,8 +15,17 @@ export function Landing() {
     }}>
         Home
     </Box>
-    <Box> 
-      <Map/>
+    <Box style= {{display: "flex", 
+    justifyContent:"center"
+    }}> 
+      <Map locations={ 
+        [
+          {name: "North Sydney", latitude:  -33.83659, longitude: 151.20080}, 
+          {name: "Newtown", latitude: -33.89988, longitude: 151.17145}, 
+          {name: "Surry Hills", latitude: -33.88444, longitude: 151.20125}, 
+          {name: "Lane Cove",latitude: -33.81801, longitude: 151.15625}
+        ]
+      }/>
     </Box>
     </>
   );
