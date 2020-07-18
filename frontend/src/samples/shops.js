e8823ea7c7daaaa6853ad02925507c5de9811013
An Thy <anthytran7@gmail.com>
2020-07-18 18:04:09 +1000

    Shop file

---
+++ frontend/src/samples/shops.js
@@ -0,0 +1,43 @@
+export const shops = [
+  shop1, shop2
+]
+
+const shop1 = {
+  name: 'Honeymeads',
+  products: [
+    {
+      name: 'Orange blossom artisan honey',
+      image: 'https://www.bbcgoodfood.com/sites/default/files/styles/push_item/public/guide/hub-image/2014/03/honey-in-a-jar-200-200.jpg?itok=mlQPJilf'
+    },
+    {
+      name: 'Honey bee pin',
+      image: 'https://images-na.ssl-images-amazon.com/images/I/5134TAb13AL.jpg'
+    },
+    {
+      name: 'Beeswax wrap',
+      image: 'https://ae01.alicdn.com/kf/H8d77298dc76f490b89013b1d032fdfd4r/reusable-bee-beeswax-wrap-bees-wax-food-wraps-3pcs-Beeswax-Wrap-Cloth-Fresh-Keeping-Sets-Food.jpg'
+    },
+    {
+      name: 'Raw honey',
+      image: 'https://saveourbees.com.au/wp-content/uploads/2017/04/sydney-raw-honey-jar-420gram.png'
+    }
+  ]   
+}
+
+const shop2 = {
+  name: 'Hand-made Teddy Bears',
+  products: [
+    {
+      name: 'Classic brown bear',
+      image: 'https://www.kmart.com.au/wcsstore/Kmart/images/ncatalog/f/3/42338673-1-f.jpg'
+    },
+    {
+      name: 'Classic purple bear',
+      image: 'https://personalisedteddybears.com.au/image/cache/catalog/Product%20Images/rosie-purple-personalised-teddy-bear-600x600.JPGg'
+    },
+    {
+      name: 'Beeswax wrap',
+      image: 'https://ae01.alicdn.com/kf/H8d77298dc76f490b89013b1d032fdfd4r/reusable-bee-beeswax-wrap-bees-wax-food-wraps-3pcs-Beeswax-Wrap-Cloth-Fresh-Keeping-Sets-Food.jpg'
+    }
+  ]   
+}
