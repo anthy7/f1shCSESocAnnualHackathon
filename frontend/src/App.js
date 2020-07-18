672cee2fb398ee742b9730f5e9244ff39864a528
nzh20 <nancyzhao999@gmail.com>
2020-07-18 21:34:25 +1000

    commiting somthgin?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,109 +6,73 @@ import SearchIcon from '@material-ui/icons/Search'
 import MoreIcon from '@material-ui/icons/MoreVert'
 import { Shops, Landing } from './screens'
 
-
 function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+      {/* <AppBar position='static'>
+        <Toolbar style={{
+          minHeight: 128,
+          padding: 4,
+        }}>
+          <Typography variant='overhead'>
+            PLAZA
+        </Typography>
+          <Typography variant='h6'>
+            <Link to='/shops'>lol</Link>
+          </Typography>
+        </Toolbar>
+      </AppBar> */}
+      <div style={{
+        root: {
+          flexGrow: 1,
+        },
+      }}>
+        <AppBar position="static">
+          <Toolbar style={{
+            minHeight: 85,
+            alignItems: 'flex-start',
+            paddingTop: 4,
+            paddingBottom: 8
+          }} >
+            <IconButton
+              style={{
+                marginRight: 8,
+              }}
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              style={{
+                flexGrow: 1,
+                alignSelf: 'flex-end',
+              }}
+              variant="h5" noWrap>
+              Plaza
+          </Typography>
+            <IconButton aria-label="search" color="inherit">
+              <SearchIcon />
+            </IconButton>
+            <IconButton aria-label="display more actions" edge="end" color="inherit">
+              <MoreIcon />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
 
-        
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
       </div>
+
+      <Switch>
+        <Route exact path='/'>
+          <Landing />
+        </Route>
+        <Route path='/shops'>
+          <Shops />
+        </Route>
+      </Switch>
     </Router>
-  );
+  )
 }
-
-// function App() {
-//   return (
-//     <Router>
-//       {/* <AppBar position='static'>
-//         <Toolbar style={{
-//           minHeight: 128,
-//           padding: 4,
-//         }}>
-//           <Typography variant='overhead'>
-//             PLAZA
-//         </Typography>
-//           <Typography variant='h6'>
-//             <Link to='/shops'>lol</Link>
-//           </Typography>
-//         </Toolbar>
-//       </AppBar> */}
-//       <div style={{
-//         root: {
-//           flexGrow: 1,
-//         },
-//       }}>
-//         <AppBar position="static">
-//           <Toolbar style={{
-//             minHeight: 85,
-//             alignItems: 'flex-start',
-//             paddingTop: 4,
-//             paddingBottom: 8
-//           }} >
-//             <IconButton
-//               style={{
-//                 marginRight: 8,
-//               }}
-//               edge="start"
-//               color="inherit"
-//               aria-label="open drawer"
-//             >
-//               <MenuIcon />
-//             </IconButton>
-//             <Typography
-//               style={{
-//                 flexGrow: 1,
-//                 alignSelf: 'flex-end',
-//               }}
-//               variant="h5" noWrap>
-//               Plaza
-//           </Typography>
-//             <IconButton aria-label="search" color="inherit">
-//               <SearchIcon />
-//             </IconButton>
-//             <IconButton aria-label="display more actions" edge="end" color="inherit">
-//               <MoreIcon />
-//             </IconButton>
-//           </Toolbar>
-//         </AppBar>
-
-//       </div>
-
-//       <Switch>
-//         <Route exact path='/'>
-//           <Landing />
-//         </Route>
-//         <Route path='/shops'>
-//           <Shops />
-//         </Route>
-//       </Switch>
-//     </Router>
-//   )
-// }
 
 export default App
